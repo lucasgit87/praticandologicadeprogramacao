9d3805d80eaff7fc4816535c073df454c2992104
Lucas Luan <lucas.c.luan1@gmail.com>
2026-03-10 20:56:13 -0300

    Update maior e menor

--- condições/maior e menor número.py
+++ condições/maior e menor número.py
@@ -1,1 +1,20 @@
+a = int(input('Digite o primeiro valor: '))
+b = int(input('Digite o segundo valor: '))
+c = int(input('Digite o terceiro valor: '))
+#verificação do menor valor
+menor = a
+if b<a and b<c:
+    menor = b
+if c<a and c<b:
+    menor = c
+
+#verificação do maior valor
+maior = a
+if b>a and b>c:
+    maior = b
+if c>a and c>b:
+    maior = c
+print(f'O menor valor digitado foi {menor}')
+print(f'O maior valor digitado foi {maior}')
+
 
